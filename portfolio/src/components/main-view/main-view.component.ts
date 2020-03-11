2ff6d286096130a0deb5b356ffeb3cc71da096c3
Cameron Stark <starkc920@outlook.com>
2020-03-11 08:03:43 -0400

    added stuff

--- portfolio/src/components/main-view/main-view.component.ts
+++ portfolio/src/components/main-view/main-view.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef, } from '@angular/core';
+import { 
+    Component, 
+    OnInit,
+    OnDestroy, 
+    ViewChild, 
+    ElementRef, 
+    AfterViewInit
+} from '@angular/core';
 import {
 
 } from '@fortawesome/free-regular-svg-icons';
@@ -13,24 +20,41 @@ export class MainViewComponent implements OnInit, OnDestroy {
 
     theme;
     //title = "</CameronStark>"
-    title = "Cameron Stark"
-    currentTheme = 'brightness_7';
+    title = "</CMS>"
+    
     navActive = false;
+    position;
     constructor(
 
     ) { 
+        //this.theme = 'colorful';
         this.theme = 'light';
     }
 
-    scrolling = false;
-    @ViewChild('content') element : ElementRef;
-    @HostListener('window:scroll', ['$event']) 
-    scrollHandler(event) {
-      console.log(this.element.nativeElement.offsetTop);
+    ngAfterViewInit() {
+       
     }
+
+
+
+    scrolling = false;
+    @ViewChild('element') contentElement : ElementRef;
+    // @HostListener('window:scroll', ['$event']) 
+    //     handleScroll() {
+        
+    //         const windowScroll = window.pageYOffset;
+    //         console.log(this.position);
+    //     }
+    scroll() {
+        this.position = this.contentElement.nativeElement.offsetTop;
+        console.log(this.position, window.pageYOffset);
+    }
+    
 
     themes = ['brightness_7', 'brightness_3', 'brightness_5'];
     themeCount = 0;
+    //themeCount = 2;
+    currentTheme = this.themes[this.themeCount];
     changeTheme() {
         this.themeCount++;
         if (this.themeCount <= 2) {
